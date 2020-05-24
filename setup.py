f7abdc953b5f881c962ab16065aa54c9836c7cc4
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-24 17:02:58 +0300

    typo

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pyeventroute',
-    version='0.0.1',
+    version='0.0.2',
     packages=[
         'pyeventroute',
     ],
     # from here all is optional
-    description='pyeventroute helps you route events to loggers or to anyother place',
-    long_description='pyeventroute helps you route events to loggers or to anyother place',
+    description='pyeventroute helps you route events to loggers or to any other place',
+    long_description='pyeventroute helps you route events to loggers or to any other place',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
     maintainer='Mark Veltzer',
