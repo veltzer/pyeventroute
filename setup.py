5c80ae71e2d69e7e2fddbe43253e250c5e1c68ee
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-04 05:13:08 +0300

    new template

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import setuptools
 
-"""
-The documentation can be found at:
-http://setuptools.readthedocs.io/en/latest/setuptools.html
-"""
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pyeventroute',
@@ -31,6 +27,8 @@ setuptools.setup(
     install_requires=[
         'pytconf',
     ],
+    extras_require={
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
