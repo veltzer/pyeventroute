1d629864ac6370db74135010b623a949c280cb63
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:21 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,6 @@
+NAME = "pyeventroute"
+DESCRIPTION_SHORT = "pyeventroute helps you route events to loggers or to any other place"
+KEYWORDS = {
+    "event",
+    "logging",
+}
--- config/project.py
+++
@@ -1,7 +0,0 @@
-""" project definitions """
-name = "pyeventroute"
-description_short = "pyeventroute helps you route events to loggers or to any other place"
-keywords = [
-    "event",
-    "logging",
-]
